60a66d9476479e551ca42ca825f8a5495c7ed684
Matt Briggs <matthew.james.briggs@gmail.com>
2025-11-11 07:46:59 -0800

    feat: add command line arguments
    Begin adding command line arguments, primarily for the sync up and sync
    down subcommands. Also set up env_logger.

--- src/args.rs
+++ src/args.rs
@@ -1,35 +1,133 @@
 //! These structs provide the CLI interface for the tiller CLI.
 
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
+use log::{error, LevelFilter};
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
-/// A tool for manipulating financial data.
-#[derive(Debug, Parser)]
+/// tiller: A command-line tool for manipulating financial data.
+///
+/// The purpose of this program is to download your financial transactions from a Tiller Google
+/// sheet (see https://tiller.com) into a local datastore. There you can manipulate tham as you
+/// wish and then sync your changes back to your Tiller sheet.
+///
+/// You will need set up a Google Docs API Key and OAuth for this. See the README at
+/// https://github.com/webern/tiller-sync for documentation on how to set this up.
+///
+/// There is also a mode in which an AI agent, like Claude or Claude Code, can use this program
+/// through the mcp subcommand.
+#[derive(Debug, Parser, Clone)]
 pub struct Args {
+    #[clap(flatten)]
+    pub(crate) common: Common,
+
     #[command(subcommand)]
     pub(crate) command: Command,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, Clone)]
 pub(crate) enum Command {
-    Import(Box<Import>),
+    /// Upload or Download Transactions, Categories and AutoCat tabs to/from your Tiller Sheet.
+    Sync(SyncArgs),
 }
 
 /// Arguments common to all subcommands.
-#[derive(Debug, Parser)]
+#[derive(Debug, Parser, Clone)]
 pub struct Common {
-    /// The directory where tiller data and configuration is held. Defaults to ~/.tiller
-    #[arg(long, env = "TILLER_HOME")]
-    home: Option<PathBuf>,
+    /// The logging verbosity. One of, from least to most verbose:
+    /// none, error, warn, info, debug, trace
+    ///
+    /// This can be overridden by RUST_LOG. See the env_logger crate for instructions.
+    #[arg(long, default_value_t = LevelFilter::Info)]
+    pub(crate) log_level: LevelFilter,
+
+    /// The directory where tiller data and configuration is held. Defaults to ~/tiller
+    #[arg(long, env = "TILLER_HOME", default_value_t = default_tiller_home())]
+    pub(crate) tiller_home: DisplayPath,
 }
 
-/// Import data from a file.
-#[derive(Debug, Parser)]
-pub(crate) struct Import {
-    /// The file to import data from.
-    #[arg(long)]
-    pub(crate) file: PathBuf,
+#[derive(Debug, Default, Copy, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+enum UpDown {
+    Up,
+    #[default]
+    Down,
+}
 
-    #[command(flatten)]
-    pub(crate) common: Common,
+serde_plain::derive_display_from_serialize!(UpDown);
+serde_plain::derive_fromstr_from_deserialize!(UpDown);
+
+/// Upload or Download Transactions, Categories and AutoCat tabs to/from your Tiller Sheet.
+#[derive(Debug, Parser, Clone)]
+pub(crate) struct SyncArgs {
+    /// The direction to sync: "up" or "down"
+    direction: UpDown,
+
+    /// The path to the Google API Key file, defaults to $TILLER_HOME/.secrets/api_key.json
+    api_key: Option<PathBuf>,
+
+    /// The path to the Google OAuth token file, defaults to $TILLER_HOME/.secrets/token.json
+    oath_token: Option<PathBuf>,
+}
+
+fn default_tiller_home() -> DisplayPath {
+    DisplayPath(match dirs::home_dir() {
+        Some(home) => home.join("tiller"),
+        None => {
+            error!(
+                "There was an error when trying to get your home directory. You can get around \
+                this by providing --tiller-home or TILLER_HOME instead of relying on the default \
+                tiller home directory. If you continue using the program right now, you may have \
+                problems!",
+            );
+            PathBuf::from("tiller")
+        }
+    })
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct DisplayPath(PathBuf);
+
+impl From<PathBuf> for DisplayPath {
+    fn from(value: PathBuf) -> Self {
+        DisplayPath(value)
+    }
+}
+
+impl Deref for DisplayPath {
+    type Target = Path;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl AsRef<Path> for DisplayPath {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
+
+impl Display for DisplayPath {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0.to_string_lossy())
+    }
+}
+
+impl FromStr for DisplayPath {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(PathBuf::from(s)))
+    }
+}
+
+impl DisplayPath {
+    pub(crate) fn _path(&self) -> &Path {
+        &self.0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use crate::args::Args;
 use crate::error::Result;
 use clap::Parser;
-use log::error;
+use env_logger::Builder;
+use log::{debug, error, trace, LevelFilter};
 use std::process::ExitCode;
 
 mod args;
@@ -10,6 +11,8 @@ mod error;
 #[tokio::main]
 async fn main() -> ExitCode {
     let args = Args::parse();
+    init_logger(args.common.log_level);
+    debug!("Log level set to {}", args.common.log_level);
 
     match main_inner(args).await {
         Ok(_) => ExitCode::SUCCESS,
@@ -21,6 +24,23 @@ async fn main() -> ExitCode {
 }
 
 pub async fn main_inner(args: Args) -> Result<()> {
-    println!("{args:?}");
+    trace!("{args:?}");
     Ok(())
+}
+
+/// Initializes the env_logger.
+pub fn init_logger(level: LevelFilter) {
+    match std::env::var(env_logger::DEFAULT_FILTER_ENV).ok() {
+        Some(_) => {
+            // RUST_LOG exists; env_logger will use it.
+            Builder::from_default_env().init();
+        }
+        None => {
+            // RUST_LOG does not exist; use default log level for this crate only.
+            Builder::new()
+                .filter(Some(env!("CARGO_CRATE_NAME")), level)
+                .filter(Some(env!("CARGO_BIN_NAME")), level)
+                .init();
+        }
+    }
 }
