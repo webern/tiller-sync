63369228aef39ebdd94fd7a448358cb4851eadff
Matt Briggs <matthew.james.briggs@gmail.com>
2025-11-11 19:18:16 -0800

    docs: edit design
    Add a section to the design doc that describes project structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 mod api;
 pub mod args;
-mod config_file;
+mod config;
 mod error;
 mod home;
 mod utils;
 
-pub use config_file::ConfigFile;
+pub use config::ConfigFile;
 pub use error::Error;
 pub use error::Result;
 pub use home::Home;
